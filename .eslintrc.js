b2aab67abca0c8ee7c9af9bf620c848c85f6b7f3
cdore <19377@farnborough.ac.uk>
2021-11-09 21:37:16 +0000

    Track eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "ignorePatterns": [".eslintrc.js", "**/vendor/*.js", "config/"],
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+    "globals": {
+        "ambientLight": true,
+        "background": true,
+        "camera": true,
+        "createCanvas": true,
+        "deltaTime": true,
+        "directionalLight": true,
+        "height": true,
+        "noStroke": true,
+        "p5": true,
+        "perspective": true,
+        "radians": true,
+        "resizeCanvas": true,
+        "shininess": true,
+        "specularMaterial": true,
+        "WEBGL": true,
+        "width": true,
+        "windowHeight": true,
+        "windowWidth": true,
+    }
+};
