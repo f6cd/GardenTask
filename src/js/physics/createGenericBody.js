9feab642cf9e08089183a6cc45f7148977699515
cdore <19377@farnborough.ac.uk>
2021-11-15 23:39:25 +0000

    Improve documentation of createGenericBody

--- src/js/physics/createGenericBody.js
+++ src/js/physics/createGenericBody.js
@@ -1,9 +1,17 @@
-import { Body } from "cannon-es";
+import { Body, Shape, Vec3 } from "cannon-es";
 
-export default function createGenericBody(colShape, mass, pos) {
+/**
+ * Create a new body with predefined settings from a collision shape, mass and position.
+ * @param {Shape} colShape Collision shape to use.
+ * @param {number} mass Mass. 
+ * @param {Vec3} pos Initial position.
+ * @returns {Body} Created physics body.
+ */
+function createGenericBody(colShape, mass, pos) {
     const body = new Body({
         mass: mass
     });
+
     body.addShape(colShape);
     body.position.copy(pos);
     body.linearDamping = 0.4;
@@ -11,3 +19,5 @@ export default function createGenericBody(colShape, mass, pos) {
 
     return body;
 }
+
+export default createGenericBody;
